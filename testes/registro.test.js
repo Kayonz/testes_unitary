98d1f32d5bb04c5928398bb309561e18e1462fa9
vitor <vitorlara261@gmail.com>
2023-09-27 20:06:06 -0300

    adicionando coisas

--- testes/registro.test.js
+++ testes/registro.test.js
@@ -4,8 +4,17 @@ describe("Este teste irá validar o registro das senhas e email ", () => {
     it("Este teste irá validar a senha se está correto", () => {
         expect(validarSenha2("2905De@!")).toBe(true)
     }) 
-    it("Este teste irá validar a senha não contém numero", () => {
+    it("Este teste irá validar se a senha não contém numero", () => {
         expect(validarSenha2("De@!jdi")).toBe(true)
+    })
+    it("Este teste irá validar se a senha não contém caracteres especiais", () => {
+        expect(validarSenha2("2905De286")).toBe(true)
+    }) 
+    it("Este teste irá validar se a senha não contém letras maiusculas", () => {
+        expect(validarSenha2("2905de12")).toBe(true)
+    })
+    it("Este teste irá validar se a senha não contém letras minusculas ", () => {
+        expect(validarSenha2("2905DEAMOR")).toBe(true)
     }) 
     
 })
